8511d3e233abd950164516150d2bc9d1f8dce5d0
omi hasan <omihasan1307@gmail.com>
2024-10-24 22:24:58 +0600

    Fix: Implemented challenges

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,21 @@
 import { UserAgentProvider } from "../components/providers/userAgentProvider";
-import "./globals.css";
 import { Layout } from "@/components/layout";
+import "./globals.css";
+import { getUserAgent } from "@/utils/getUserAgent";
 
-const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const RootLayout = async ({
+  children,
+  request,
+}: {
+  children: React.ReactNode;
+  request: Request;
+}) => {
+  const userAgent = getUserAgent(request?.headers);
+
   return (
     <html lang="en">
       <body>
-        <UserAgentProvider>
+        <UserAgentProvider userAgent={userAgent}>
           <Layout>{children}</Layout>
         </UserAgentProvider>
       </body>
--- src/views/products/products.tsx
+++ src/views/products/products.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Product } from "@/types";
 import { ProductModal } from "@/views/products/productModal/productModal";
 import { BackToHome } from "@/components/backToHome/backToHome";
@@ -8,9 +8,13 @@ import { ProductList } from "@/views/products/productList/productList";
 import { PaginationControls } from "@/views/products/paginationControls/paginationControls";
 import { usePagination } from "@/hooks/usePagination";
 import { PRODUCTS_DATA } from "@/data/productsData";
+import { useSearchParams, useRouter } from "next/navigation";
 
 export const Products: React.FC = () => {
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   const {
     currentPage,
     totalPages,
@@ -18,13 +22,49 @@ export const Products: React.FC = () => {
     handlePageChange,
   } = usePagination({ items: PRODUCTS_DATA, itemsPerPage: 5 });
 
-  const handleOpenModal = useCallback((product: Product) => {
-    setSelectedProduct(product);
-  }, []);
+  useEffect(() => {
+    const productId = searchParams.get("productId");
+    if (productId) {
+      const product = PRODUCTS_DATA.find((p) => p.id === productId);
+      if (product) {
+        setSelectedProduct(product);
+      }
+    } else {
+      setSelectedProduct(null);
+    }
+  }, [searchParams]);
+
+  const handleOpenModal = useCallback(
+    (product: Product) => {
+      setSelectedProduct(product);
+      router.push(`?productId=${product.id}`);
+    },
+    [router]
+  );
 
   const handleCloseModal = useCallback(() => {
     setSelectedProduct(null);
-  }, []);
+    router.push("/products");
+  }, [router]);
+
+  useEffect(() => {
+    const handlePopState = () => {
+      const productId = searchParams.get("productId");
+      if (productId) {
+        const product = PRODUCTS_DATA.find((p) => p.id === productId);
+        if (product) {
+          setSelectedProduct(product);
+        }
+      } else {
+        setSelectedProduct(null);
+      }
+    };
+
+    window.addEventListener(" ", handlePopState);
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, [searchParams]);
 
   return (
     <div>
